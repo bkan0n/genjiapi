aabe6a1a99a3f8d31c5209cfd9f440f191dba8b7
Ty Lovejoy <ty@fromskytocenter.com>
2024-12-22 14:45:20 -0600

    fix: gen api json final

--- scripts/generate_openapi.py
+++ scripts/generate_openapi.py
@@ -11,7 +11,7 @@ import app  # Import your Litestar app instance
 openapi_schema_dict = app.app.openapi_schema.to_schema()
 
 # Save the OpenAPI schema as a JSON file
-with open("../openapi.json", "w") as file:
+with open("openapi.json", "w") as file:
     file.write(json.dumps(openapi_schema_dict, indent=2))
 
 print("OpenAPI schema successfully generated and saved to openapi.json.")
